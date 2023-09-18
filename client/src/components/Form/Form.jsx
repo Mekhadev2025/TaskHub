948ebcf2a59a5a7c4cbc86056ecf8e447b9e13de
Mekha <mekhalnair@gmail.com>
2023-09-18 23:59:44 +0530

    adaaaa

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import '../Form/Form.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "../Form/Form.css";
 
 const Form = (props) => {
-  const [taskName, setTask] = useState('');
+  const [taskName, setTask] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    axios.post('http://localhost:5000/add', { taskName }) // Send taskName as an object
-      .then((response) => { // Fix the .then block
-        console.log('Response:', response.data);
-        props.updateData(taskName)
-       
-      })
-      .catch((error) => { // Handle errors in the .catch block
-        console.error('Error:', error);
-         
-      });
-      setTask("")
 
+    axios
+      .post("http://localhost:5000/add", { taskName }) // Send taskName as an object
+      .then((response) => {
+        // Fix the .then block
+        console.log("Response:", response.data);
+        props.updateData(taskName);
+      })
+      .catch((error) => {
+        // Handle errors in the .catch block
+        console.error("Error:", error);
+      });
+    setTask("");
   };
 
   return (
