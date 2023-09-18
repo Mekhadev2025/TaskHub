febe7cfc3a91b60809d9fc94fbcde455e3abba6f
Mekha <mekhalnair@gmail.com>
2023-09-18 19:49:15 +0530

    adding server basic

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
-npm
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Running Successfully on Port Number ${PORT}`);
+});
+
